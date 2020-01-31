69664810a8ecfb74636ca9ba0128a0abd8c972b1
liamylian <liamylian@163.com>
2020-01-31 11:21:06 +0800

    pack

--- binary.go
+++ binary.go
@@ -8,18 +8,27 @@ import (
 )
 
 func Pack(v interface{}) ([]byte, error) {
+	type fieldByteIndex struct {
+		start  uint64
+		end    uint64
+		endian binary.ByteOrder
+	}
+
 	typ := reflect.TypeOf(v)
 	val := reflect.ValueOf(v)
 	if typ.Kind() != reflect.Struct {
 		return nil, errors.New("not a struct")
 	}
 
+	sizeofTagItems := make(map[string]*tagItemSizeOf)
+	fieldBytes := make(map[string]fieldByteIndex)
 	buffer := bytes.NewBuffer(nil)
 	for i := 0; i < val.NumField(); i++ {
 		fieldType := typ.Field(i)
 		fieldTag := fieldType.Tag
+		filedName := fieldType.Name
 		fieldValue := val.Field(i)
-		tagEndian, tagSize, _, err := getTagItems(fieldTag.Get("binary"))
+		tagEndian, tagSize, tagSizeof, err := getTagItems(fieldTag.Get("binary"))
 		if err != nil {
 			return nil, err
 		}
@@ -28,6 +37,9 @@ func Pack(v interface{}) ([]byte, error) {
 		}
 		if tagSize == nil {
 			tagSize = &tagItemSize{0}
+		}
+		if tagSizeof != nil {
+			sizeofTagItems[filedName] = tagSizeof
 		}
 
 		var bytes []byte
@@ -46,13 +58,28 @@ func Pack(v interface{}) ([]byte, error) {
 		if _, err := buffer.Write(bytes); err != nil {
 			return nil, err
 		}
+
+		byteEnd := buffer.Len()
+		byteStart := byteEnd - len(bytes)
+		fieldBytes[filedName] = fieldByteIndex{uint64(byteStart), uint64(byteEnd), tagEndian.Endian}
+	}
+	resultBytes := buffer.Bytes()
+
+	for fieldName, sizeofTagItem := range sizeofTagItems {
+		sumSize := uint64(0)
+		sumSizeLen := uint64(8)
+		for _, fieldName := range sizeofTagItem.Fields {
+			byteIndex := fieldBytes[fieldName]
+			sumSize += byteIndex.end - byteIndex.start
+		}
+		byteIndex := fieldBytes[fieldName]
+		byteLen := byteIndex.end - byteIndex.start
+		sumSizeBytes := make([]byte, sumSizeLen)
+		byteIndex.endian.PutUint64(sumSizeBytes, sumSize)
+		copy(resultBytes[byteIndex.start:byteIndex.end], sumSizeBytes[sumSizeLen-byteLen:])
 	}
 
-	return buffer.Bytes(), nil
-}
-
-func Unpack(v interface{}, bytes []byte) error {
-	panic("not implemented")
+	return resultBytes, nil
 }
 
 func pack(v reflect.Value, endian binary.ByteOrder, size int) ([]byte, error) {
@@ -67,15 +94,20 @@ func pack(v reflect.Value, endian binary.ByteOrder, size int) ([]byte, error) {
 			return nil, err
 		}
 		bytes := buffer.Bytes()
+		byteSize := len(bytes)
 		if size == 0 {
 			return bytes, nil
-		} else if byteSize := len(bytes); size > byteSize {
+		} else if size > byteSize {
 			padding := make([]byte, size-byteSize, size)
 			return append(padding, bytes...), nil
 		} else {
-			return bytes[:size], nil
+			return bytes[byteSize-size:], nil
 		}
 	default:
 		return nil, errors.New("not supported kind")
 	}
+}
+
+func Unpack(v interface{}, bytes []byte) error {
+	panic("not implemented")
 }
